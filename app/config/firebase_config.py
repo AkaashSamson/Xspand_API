d9116e40c540f7fd01e47e2404ec90a6420976fe
AkaashSamson <akaashsam22@gmail.com>
2025-02-23 16:48:17 +0530

    No env

--- app/config/firebase_config.py
+++ app/config/firebase_config.py
@@ -1,11 +1,11 @@
 from firebase_admin import credentials, initialize_app
-from dotenv import load_dotenv
+# from dotenv import load_dotenv
 import os
 import base64
 import json
 
 def init_firebase():
-    load_dotenv()
+    # load_dotenv()
     # Decode the base64 string and parse it as JSON
     firebase_config_cred = json.loads(base64.b64decode(os.getenv("FIREBASE_CONFIG_CRED")).decode('utf-8'))
     cred = credentials.Certificate(firebase_config_cred)
